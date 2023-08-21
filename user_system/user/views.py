295128fedfde25f874556aaef1f131e1bfdbf76e
heartless45 <139840257+heartless45@users.noreply.github.com>
2023-08-21 11:28:26 +0530

    Delete user_system directory

--- user_system/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import logout
-from .models import User
-
-
-
-# Create your views here.
-def home_view(request):
-    return render(request,'home.html')
